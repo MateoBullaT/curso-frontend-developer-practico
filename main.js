768db1052c7b6b14f140da85a06a11a64c49f4cb
Mateo BullaT <mateobulla2000@gmail.com>
2024-01-02 19:45:39 -0500

    Shopping cart add

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ nameUser.addEventListener("click", toggleDesktopMenu);
 
 function toggleDesktopMenu(){
     desktopMenu.classList.toggle("inactive");
+    shoppingCart.classList.add("inactive");
 };
 
 
@@ -17,4 +18,17 @@ menuToggle.addEventListener("click", toggleMobileMenu);
 
 function toggleMobileMenu() {    
     mobileMenu.classList.toggle("toggle-active");
+    shoppingCart.classList.add("inactive")
 };
+
+//SHOPPING CART
+const cartIcon = document.querySelector(".navbar-shopping-cart");
+const shoppingCart = document.querySelector(".product-detail");
+cartIcon.addEventListener("click", showShoppingCart); 
+
+function showShoppingCart(){
+    mobileMenu.classList.add("inactive");
+    mobileMenu.classList.remove("toggle-active");
+    shoppingCart.classList.toggle("inactive");
+    desktopMenu.classList.add("inactive");
+}
