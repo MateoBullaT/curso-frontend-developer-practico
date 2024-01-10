89659d3ca4d85adbf6bc36ee4909330283c0675d
Mateo BullaT <mateobulla2000@gmail.com>
2024-01-09 22:23:59 -0500

    dinamic list products rendering JS

--- main.js
+++ main.js
@@ -32,3 +32,71 @@ function showShoppingCart(){
     shoppingCart.classList.toggle("inactive");
     desktopMenu.classList.add("inactive");
 }
+
+
+
+//LIST PRODUCTS RENDERING
+
+const productList = [];
+
+productList.push({
+    name: "Tostadora de Lujo",
+    price: 25,
+    image: "https://images.pexels.com/photos/7936635/pexels-photo-7936635.jpeg?auto=compress&cs=tinysrgb&w=600",
+});
+
+productList.push({
+    name: "Tostadora premium",
+    price: 100,
+    image: "https://images.pexels.com/photos/7936636/pexels-photo-7936636.jpeg?auto=compress&cs=tinysrgb&w=600"
+});
+
+productList.push({
+    name: "Bicicleta mamalona",
+    price: 270,
+    image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+});
+
+
+
+
+
+function productListRendering(arr){
+    let productsContainer = document.querySelector(".cards-container");
+    for (const product of arr) {
+
+        let productCard = document.createElement("div");
+        productCard.classList.add("product-card");
+        productsContainer.append(productCard);
+        
+        let imageProduct = document.createElement("img");
+        productCard.classList.add("product-card");
+        imageProduct.setAttribute("src", product.image);
+
+        let infoContainer = document.createElement("div");
+        infoContainer.classList.add("product-info");
+
+        let detailsContainer = document.createElement("div");
+        
+        let priceProduct = document.createElement("p");
+        priceProduct.innerText = "$" + product.price;
+        let nameProduct = document.createElement("p");
+        nameProduct.innerText = product.name;
+
+        let iconContainer = document.createElement("figure");
+        let cartIcon = document.createElement("img");
+        cartIcon.setAttribute("src", "./icons/bt_add_to_cart.svg");
+        
+        iconContainer.append(cartIcon);
+        
+        detailsContainer. append(priceProduct, nameProduct);
+
+        infoContainer.append(detailsContainer, iconContainer);
+
+        productCard.append(imageProduct, infoContainer);
+    }
+};
+
+productListRendering(productList);
+
+
